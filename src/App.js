6d73134929cb8541850ced3bb83e9492cf8d2dbe
dieob <dbaezrenault@gmail.com>
2020-04-25 17:52:54 -0400

    basic layout of page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  
+  constructor(props) {
+    super(props);
+    this.state = {posts:[]}
+  }
+
+  componentDidMount(){
+    //fetch posts from api
+    fetch('http://heroku-java-webapp.herokuapp.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({posts: data }));
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <Header></Header>
+        <Navigation></Navigation>
+        <PostList></PostList>
+      </div>
+    );
+  }
 }
+
+class Header extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <Logo></Logo>
+        <Search></Search>
+        <Settings></Settings>
+      </div>
+    );
+  }
+
+}
+
+class Logo extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="logo-box">
+        Logo
+      </div>
+    );
+  }
+
+}
+
+class Search extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="search-box">
+        <input className="input-box" type="text" placeholder="Search something"></input>
+      </div>
+    );
+  }
+}
+
+class Settings extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="settings-box">
+        Settings
+      </div>
+    );
+  }
+}
+
+class Navigation extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="navigation">
+        <Button></Button>
+        <h3>TEST</h3>
+        <h3>TEST</h3>
+        <h3>TEST</h3>
+        <h3>TEST</h3>
+      </div>
+    );
+  }
+
+}
+
+class PostList extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="post-list">
+        <h3>posts</h3>
+      </div>
+    );
+  }
+
+}
+
+class Button extends React.Component{
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="button-container">
+        <div className="button-primary">
+          Post yours
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
 
 export default App;
